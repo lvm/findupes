2643a5c4aa40741b4a07dc565c5a49a5889c3d8a
Mauro <mauro@sdf.org>
2025-02-05 11:43:03 -0300

    comparing people

--- main.go
+++ main.go
@@ -40,6 +40,57 @@ func (p Person) FullName() string {
 	return fmt.Sprintf("%s %s", p.name, p.lastName)
 }
 
+func getScore(person, other Person) float64 {
+	if person.ID == other.ID {
+		return 1.0
+	}
+
+	values := map[csv.Column]float64{
+		name:     0.25,
+		lastName: 0.25,
+		email:    0.3,
+		zip:      0.1,
+		addr:     0.1,
+	}
+
+	var score float64
+
+	if person.name == other.name {
+		score += values[name]
+	}
+
+	if person.lastName == other.lastName {
+		score += values[lastName]
+	}
+
+	if person.Email == other.Email {
+		score += values[email]
+	}
+
+	if person.Zip == other.Zip {
+		score += values[zip]
+	}
+
+	if person.Address == other.Address {
+		score += values[addr]
+	}
+
+	return score
+}
+
+func getAccuracy(score float64) string {
+	switch {
+	case score > 0.25 && score < 0.5:
+		return "Low"
+	case score > 0.25 && score < 0.5:
+		return "Mid"
+	case score > 0.25 && score < 0.5:
+		return "High"
+	default:
+		return ""
+	}
+}
+
 func main() {
 	filename := flag.String("file", "", "CSV Filename")
 	flag.Parse()
@@ -55,9 +106,18 @@ func main() {
 		return
 	}
 
+	people := make([]Person, 0)
 	for _, row := range rows {
-		p := NewPerson(row)
-		fmt.Println(p.ID, p.FullName(), p.Email)
+		people = append(people, NewPerson(row))
+	}
+
+	for _, p := range people {
+		for _, other := range people {
+			score := getScore(p, other)
+			if accuracy := getAccuracy(score); accuracy != "" {
+				fmt.Println(p.ID, p.FullName(), p.Email, "|", other.ID, other.FullName(), other.Email, "=", accuracy, fmt.Sprintf("(%.2f)", score))
+			}
+		}
 	}
 
 }
