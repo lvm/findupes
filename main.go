78804e208b66d0e0d1d114c7b5a40ccb1318c796
Mauro <mauro@sdf.org>
2025-02-05 11:16:30 -0300

    adding a better csv reader

--- main.go
+++ main.go
@@ -2,44 +2,87 @@ package main
 
 import (
 	"encoding/csv"
+	"errors"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
+
+type Column string
+type Header []Column
+type Row map[Column]string
+
+const (
+	id       Column = "contactID"
+	name     Column = "name"
+	lastName Column = "name1"
+	email    Column = "email"
+	zip      Column = "postalZip"
+	addr     Column = "address"
+)
+
+var ErrNotEnoughData = errors.New("csv seems to be empty")
+
+func NewHeader(cols ...string) Header {
+	header := make(Header, len(cols))
+
+	for i, col := range cols {
+		header[i] = Column(col)
+	}
+
+	return header
+}
+
+func csvReader(filename string) ([]Row, error) {
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	lines, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(lines) < 2 {
+		return nil, ErrNotEnoughData
+	}
+
+	headers := NewHeader(lines[0]...)
+
+	var rows []Row
+	for _, line := range lines[1:] {
+		row := make(Row)
+		for i, value := range line {
+			row[headers[i]] = value
+		}
+		rows = append(rows, row)
+	}
+
+	return rows, nil
+}
 
 func main() {
 	filename := flag.String("file", "", "CSV Filename")
 	flag.Parse()
 
-	file, err := os.Open(*filename)
+	if *filename == "" {
+		log.Println("[Error] Missing `filename`")
+		return
+	}
+
+	rows, err := csvReader(*filename)
 	if err != nil {
-		return
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	rows, err := reader.ReadAll()
-	if err != nil {
+		log.Println("[Error] Reading CSV Failed: %v", err)
 		return
 	}
 
-	if len(rows) < 2 {
-		return
-	}
-
-	headers := rows[0]
-
-	var lines []map[string]string
-	for _, row := range rows[1:] {
-		line := make(map[string]string)
-		for i, value := range row {
-			line[headers[i]] = value
-		}
-		lines = append(lines, line)
-	}
-
-	for _, line := range lines {
-		fmt.Println(line)
+	for _, row := range rows {
+		fmt.Println(row[id], row[name], row[lastName], row[email])
 	}
 
 }
