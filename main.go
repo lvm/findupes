76f64ec8cbe7125359639244e3b7dc235dda9188
Mauro <mauro@sdf.org>
2025-02-05 11:21:29 -0300

    moving csv stuff to its own pkg

--- main.go
+++ main.go
@@ -1,70 +1,21 @@
 package main
 
 import (
-	"encoding/csv"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"os"
-)
 
-type Column string
-type Header []Column
-type Row map[Column]string
+	"github.com/lvm/findupes/pkg/csv"
+)
 
 const (
-	id       Column = "contactID"
-	name     Column = "name"
-	lastName Column = "name1"
-	email    Column = "email"
-	zip      Column = "postalZip"
-	addr     Column = "address"
+	id       csv.Column = "contactID"
+	name     csv.Column = "name"
+	lastName csv.Column = "name1"
+	email    csv.Column = "email"
+	zip      csv.Column = "postalZip"
+	addr     csv.Column = "address"
 )
-
-var ErrNotEnoughData = errors.New("csv seems to be empty")
-
-func NewHeader(cols ...string) Header {
-	header := make(Header, len(cols))
-
-	for i, col := range cols {
-		header[i] = Column(col)
-	}
-
-	return header
-}
-
-func csvReader(filename string) ([]Row, error) {
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	lines, err := reader.ReadAll()
-	if err != nil {
-		return nil, err
-	}
-
-	if len(lines) < 2 {
-		return nil, ErrNotEnoughData
-	}
-
-	headers := NewHeader(lines[0]...)
-
-	var rows []Row
-	for _, line := range lines[1:] {
-		row := make(Row)
-		for i, value := range line {
-			row[headers[i]] = value
-		}
-		rows = append(rows, row)
-	}
-
-	return rows, nil
-}
 
 func main() {
 	filename := flag.String("file", "", "CSV Filename")
@@ -75,9 +26,9 @@ func main() {
 		return
 	}
 
-	rows, err := csvReader(*filename)
+	rows, err := csv.Reader(*filename)
 	if err != nil {
-		log.Println("[Error] Reading CSV Failed: %v", err)
+		log.Printf("[Error] Reading CSV Failed: %v", err)
 		return
 	}
 
